0a1816e369ba66032f98a99d025e1f7312aa0f9b
Rui Nian <rnian@ualberta.ca>
2018-12-27 00:57:25 -0700

    updates

--- NeuralNet.py
+++ NeuralNet.py
@@ -3,14 +3,13 @@ Artificial Neural Network Code v1.0  (Feed-forward neural network)
 
 By:  Rui Nian
 
-Date of last edit: December 26th, 2018
+Date of last edit: December 27th, 2018
 
 Patch notes: -
 
 Known issues: -
 
-shuffle, normalize
-to beat: precision = 96.1%, recall = 94.6%
+Features:  normalization, shuffle, prec, recall, testing
 """
 
 
@@ -29,6 +28,40 @@ import os
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
+
+# Min max normalization
+class MinMaxNormalization:
+    """
+    Inputs
+       -----
+            data:  Input feature vectors from the training data
+
+    Attributes
+       -----
+         col_min:  The minimum value per feature
+         col_max:  The maximum value per feature
+     denominator:  col_max - col_min
+
+     Methods
+        -----
+     init:  Builds the col_min, col_max, and denominator
+     call:  Normalizes data based on init attributes
+    """
+
+    def __init__(self, data):
+        self.col_min = np.min(data, axis=0).reshape(1, data.shape[1])
+        self.col_max = np.max(data, axis=0).reshape(1, data.shape[1])
+        self.denominator = abs(self.col_max - self.col_min)
+
+        # Fix divide by zero, replace value with 1 because these usually happen for boolean columns
+        for index, value in enumerate(self.denominator):
+            if value[0] == 0:
+                self.denominator[index] = 1
+
+    def __call__(self, data):
+        return np.divide((data - self.col_min), self.denominator)
+
+
 df = pd.read_csv("test_datasets/breast-cancer-wisconsin.data.txt")
 # Remove missing values
 df.replace('?', np.nan, inplace=True)
@@ -46,14 +79,18 @@ df.loc[:, 'class'].replace(4, 1, inplace=True)
 df.sort_values(['class'], ascending=False, inplace=True)
 df.reset_index(drop=True, inplace=True)
 df = df.values
+np.random.shuffle(df)
 
 features = df[:, 0:-1]
 labels = df[:, -1].reshape(features.shape[0], 1)
 
+min_max_normalization = MinMaxNormalization(features)
+features = min_max_normalization(features)
+
 input_size = features.shape[1]
-nodes_h1 = 10
-nodes_h2 = 10
-nodes_h3 = 10
+nodes_h1 = 20
+nodes_h2 = 20
+nodes_h3 = 20
 output_size = 1
 
 batch_size = 32
@@ -108,16 +145,17 @@ recall, recall_ops = tf.metrics.recall(labels=y, predictions=pred)
 init = tf.global_variables_initializer()
 init_l = tf.local_variables_initializer()
 
-epochs = 200
+epochs = 1000
 loss_history = []
 
 saver = tf.train.Saver()
 
 with tf.Session() as sess:
-    sess.run(init)
-    sess.run(init_l)
 
     # saver.restore(sess, 'checkpoints/test.ckpt')
+
+    sess.run(init)
+    sess.run(init_l)
 
     for epoch in range(epochs):
 
@@ -130,14 +168,15 @@ with tf.Session() as sess:
             current_loss = sess.run(loss, feed_dict={X: batch_X, y: batch_y})
             loss_history.append(current_loss)
 
-            if i % 10 == 0:
+            if i % 18 == 0 and i != 0:
                 Acc = sess.run(accuracy, feed_dict={X: batch_X, y: batch_y})
                 Prec, Recall = sess.run([prec_ops, recall_ops], feed_dict={X: batch_X, y: batch_y})
                 print('Acc: {:5f} | Prec: {:5f} | Recall: {:5f}'.format(Acc, Prec, Recall))
 
             # predictions = sess.run(pred, feed_dict={X: batch_X, y: batch_y})
-            # print(predictions)
-            # print(batch_y)
+            # Acc = sess.run(accuracy, feed_dict={X: features, y: labels})
+            # Prec, Recall = sess.run([prec_ops, recall_ops], feed_dict={X: features, y: labels})
+            # print('Acc: {:5f} | Prec: {:5f} | Recall: {:5f}'.format(Acc, Prec, Recall))
             # break
 
     saver.save(sess, 'checkpoints/test.ckpt')
