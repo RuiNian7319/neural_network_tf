b6267ee5e5a018aeb1b4f9a326bf0d5682de4cb5
Rui Nian <rnian@ualberta.ca>
2019-01-02 15:29:54 -0700

    NN performance metrics added

--- neural_networks_reg/NeuralNetReg.py
+++ neural_networks_reg/NeuralNetReg.py
@@ -1,11 +1,12 @@
 """
-Artificial Neural Network Regression Code v1.0 (Feed-forward neural network)
+Artificial Neural Network Regression Code v1.1 (Feed-forward neural network)
 
 By: Rui Nian
 
-Date of last edit: December 31st, 2018
+Date of last edit: January 2nd, 2018
 
-Patch Notes: -
+Patch Notes: Added RMSE, R2
+             Added Train / Test Accuracy during runtime
 
 Known Issues: -
 
@@ -66,12 +67,15 @@ class MinMaxNormalization:
     def __call__(self, data):
         return np.divide((data - self.col_min), self.denominator)
 
+    def unnormalize(self, data):
+        return np.multiply((data + self.col_min), self.denominator)
+
 
 # Load data
-path = '/Users/ruinian/Documents/Willowglen/valve_pressure_data/'
-# path = '/home/rui/Documents/logistic_regression_tf/'
+# path = '/Users/ruinian/Documents/Willowglen/valve_pressure_data/'
+path = '/home/rui/Documents/Willowglen/'
 
-raw_data = pd.read_csv(path + 'valve_pressure_data.csv')
+raw_data = pd.read_csv(path + 'valve_pressure_data/valve_pressure_data_omit_all_outliers.csv')
 raw_data.sort_values(by=['175642874_630'], inplace=True)
 raw_data = raw_data.values
 
@@ -80,11 +84,20 @@ assert(not np.isnan(raw_data).any())
 train_X, test_X, train_y, test_y = train_test_split(raw_data[:, 1], raw_data[:, 0],
                                                     test_size=0.1, random_state=42, shuffle=True)
 
+# Reshaping data to ensure compatibility with tensorflow
 train_X = train_X.reshape(-1, 1)
 test_X = test_X.reshape(-1, 1)
 
 train_y = train_y.reshape(-1, 1)
 test_y = test_y.reshape(-1, 1)
+
+# Normalization of input space
+pickle_in = open(path + 'neural_net_tf/pickles/norm_reg.pickle', 'rb')
+min_max_normalization = pickle.load(pickle_in)
+
+# min_max_normalization = MinMaxNormalization(train_X)
+train_X = min_max_normalization(train_X)
+test_X = min_max_normalization(test_X)
 
 input_size = train_y.shape[1]
 h1_nodes = 30
@@ -92,14 +105,14 @@ h2_nodes = 30
 h3_nodes = 30
 output_size = 1
 
-batch_size = 512
+batch_size = 2048
 total_batch_number = int(train_X.shape[0] / batch_size)
 
 X = tf.placeholder(dtype=tf.float32, shape=[None, input_size])
 y = tf.placeholder(dtype=tf.float32, shape=[None, output_size])
 
 # Batch normalization
-training = True
+training = False
 is_train = tf.placeholder(dtype=tf.bool, name='is_train')
 
 hidden_layer_1 = {'weights': tf.get_variable('h1_weights', shape=[input_size, h1_nodes],
@@ -137,40 +150,95 @@ l3 = tf.layers.batch_normalization(l3, training=is_train)
 output = tf.add(tf.matmul(l3, output_layer['weights']), output_layer['biases'])
 
 # L2 Regularization
-lambd = 0.000
+lambd = 0.01
 trainable_vars = tf.trainable_variables()
 lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in trainable_vars if 'bias' not in v.name])
 
-loss = tf.reduce_mean(tf.losses.mean_squared_error(labels=y, predictions=output))
+loss = tf.math.sqrt(tf.reduce_mean(tf.losses.mean_squared_error(labels=y, predictions=output)))
 
 # Batch Normalization
 update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 with tf.control_dependencies(update_ops):
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.0005).minimize(loss)
+
+# Evaluation metrics
+RMSE = tf.reduce_mean(tf.math.square(tf.math.subtract(y, output)))
+MSE = tf.reduce_sum(tf.math.square(tf.math.subtract(y, output)))
+Syy = tf.reduce_sum(tf.math.square(tf.math.subtract(y, tf.reduce_mean(y))))
+R2 = 1 - np.divide(MSE, Syy)
 
 init = tf.global_variables_initializer()
 
-epochs = 5
+epochs = 1
 loss_history = []
 
 saver = tf.train.Saver()
 
 with tf.Session() as sess:
 
-    sess.run(init)
+    saver.restore(sess, path + 'neural_net_tf/checkpoints/test_reg.ckpt')
 
-    for epoch in range(epochs):
+    # sess.run(init)
 
-        for i in range(total_batch_number):
-            batch_index = i * batch_size
-            batch_X = train_X[batch_index:batch_index + batch_size, :]
-            batch_y = train_y[batch_index:batch_index + batch_size, :]
+    # for epoch in range(epochs):
+    #
+    #     for i in range(total_batch_number):
+    #
+    #         # If the batch size is too big for the last bit of data
+    #         if i == (int(raw_data.shape[0] / batch_size) + 1):
+    #             batch_X = train_X[int(raw_data.shape[0] / batch_size):]
+    #             batch_y = train_y[int(raw_data.shape[0] / batch_size):]
+    #
+    #         batch_index = i * batch_size
+    #         batch_X = train_X[batch_index:batch_index + batch_size, :]
+    #         batch_y = train_y[batch_index:batch_index + batch_size, :]
+    #
+    #         sess.run(optimizer, feed_dict={X: batch_X, y: batch_y, is_train: training})
+    #         current_loss = sess.run(loss, feed_dict={X: batch_X, y: batch_y, is_train: training})
+    #         loss_history.append(current_loss)
+    #
+    #         if i % 100 == 0:
+    #             train_RMSE = sess.run(RMSE, feed_dict={X: train_X, y: train_y, is_train: training})
+    #             test_RMSE = sess.run(RMSE, feed_dict={X: test_X, y: test_y, is_train: training})
+    #             print('Epoch: {} | Loss: {:5f} | Train RMSE: {:5f} | Test RMSE: {:5f}'.format(epoch, current_loss,
+    #                                                                                           train_RMSE, test_RMSE))
 
-            sess.run(optimizer, feed_dict={X: batch_X, y: batch_y, is_train: training})
-            current_loss = sess.run(loss, feed_dict={X: batch_X, y: batch_y, is_train: training})
-            loss_history.append(current_loss)
+    # Save Model
+    # saver.save(sess, path + 'neural_net_tf/checkpoints/test_reg.ckpt')
 
-            if i % 50 == 0:
-                print('Epoch: {} | Loss: {}'.format(epoch, current_loss))
+    # Overall performance metrics
+    overall_X = raw_data[:, 1].reshape(-1, 1)
+    overall_X = min_max_normalization(overall_X)
+    overall_y = raw_data[:, 0].reshape(-1, 1)
 
-    predictions = sess.run(output, feed_dict={X: test_X, y: test_y, is_train: training})
+    # predictions = sess.run(output, feed_dict={X: overall_X, y: overall_y,
+    #                                           is_train: training})
+
+    # Evaluation, broken into pieces because of batch normalization
+    predictions = tf.constant([], shape=(0, 1))
+    for i in range(int(raw_data.shape[0] / batch_size) + 1):
+
+        # If the batch size is too big for the last bit of data
+        if i == (int(raw_data.shape[0] / batch_size) + 1):
+            batch_X = overall_X[int(raw_data.shape[0] / batch_size):]
+            batch_y = overall_y[int(raw_data.shape[0] / batch_size):]
+
+        batch_index = i * batch_size
+        batch_X = overall_X[batch_index:batch_index + batch_size, :]
+        batch_y = overall_y[batch_index:batch_index + batch_size, :]
+
+        prediction = sess.run(output, feed_dict={X: batch_X, y: batch_y,
+                                                 is_train: training})
+        predictions = tf.concat([predictions, prediction], axis=0)
+
+    predictions = sess.run(predictions)
+
+    print('Overall R2: {:5f} | Overall RMSE: {:5f}'.format(sess.run(R2, feed_dict={X: overall_X,
+                                                                                   y: overall_y,
+                                                                                   is_train: training}),
+                                                           sess.run(RMSE, feed_dict={X: overall_X,
+                                                                                     y: overall_y,
+                                                                                     is_train: training})))
+
+    # Unnormalize the features
+    overall_X = min_max_normalization.unnormalize(overall_X)
